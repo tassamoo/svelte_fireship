20a84e7b45136f2e41c0e5726046e49049975dd3
tassamoo <tassamu@outlook.com>
2022-04-12 17:00:50 +0700

    create card components and delete function

---
+++ svelte-feedback-app/src/components/Card.svelte
@@ -0,0 +1,17 @@
+<div class="card">
+    <slot>
+
+    </slot>
+</div>
+
+<style>
+    .card{
+        background-color: #fff;
+        color: #121212;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+
+    }
+</style>
